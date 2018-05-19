fff05070eee84f2ab6ec10af0283075afa890b91
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-19 07:45:51 +0100

    Adds Person abstract class

---
+++ src/main/java/models/Person.java
@@ -0,0 +1,39 @@
+package models;
+
+public abstract class Person {
+
+    private int id;
+    private String name;
+    private Set<Film> films;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public Person() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Set<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(Set<Film> films) {
+        this.films = films;
+    }
+}
