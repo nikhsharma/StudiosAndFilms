5dab7d7fb4e327ad4daac1e06f38c8221d0ba28a
Nikhil Sharma <nikhil_sharma@live.co.uk>
2018-05-19 07:48:36 +0100

    Adds Director class

---
+++ src/main/java/models/People/Director.java
@@ -0,0 +1,11 @@
+package models.People;
+
+public class Director extends Person {
+
+    public Director(String name) {
+        super(name);
+    }
+
+    public Director() {
+    }
+}
--- src/main/java/models/People/Person.java
+++ src/main/java/models/People/Person.java
@@ -1,4 +1,4 @@
-package models;
+package models.People;
 
 public abstract class Person {
 
